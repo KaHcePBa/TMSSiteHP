6638345f9838af364becd0e11205b84919c20b06
KaHcePBa <61241758+KaHcePBa@users.noreply.github.com>
2020-03-30 20:12:22 +0300

    ver.1.0.7

--- src/project/urls.py
+++ src/project/urls.py
@@ -6,31 +6,31 @@ from django.urls import path
 here = Path(__file__).parent.resolve()
 
 
-def view_index():
+def view_index(r):
     index = here.parent.parent / "index.html"
     with index.open() as src:
         return HttpResponse(src.read())
 
 
-def view_resume():
+def view_resume(r):
     resume = here.parent.parent / "resume.html"
     with resume.open() as src:
         return HttpResponse(src.read())
 
 
-def view_projects():
+def view_projects(r):
     projects = here.parent.parent / "projects.html"
     with projects.open() as src:
         return HttpResponse(src.read())
 
 
-def view_thoughts():
+def view_thoughts(r):
     thoughts = here.parent.parent / "thoughts.html"
     with thoughts.open() as src:
         return HttpResponse(src.read())
 
 
-def view_image():
+def view_image(r):
     jpg_me = here.parent.parent / "images/me.jpg"
     with jpg_me.open("rb") as src:
         return HttpResponse(src.read(), content_type="image/jpeg")
