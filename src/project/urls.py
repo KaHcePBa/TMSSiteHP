46f41da0c36e42f259bb52ab903db342e840b8e2
KaHcePBa <61241758+KaHcePBa@users.noreply.github.com>
2020-03-30 19:56:25 +0300

    ver.1.0.5

--- src/project/urls.py
+++ src/project/urls.py
@@ -6,13 +6,41 @@ from django.urls import path
 here = Path(__file__).parent.resolve()
 
 
-def view(r):
+def view_index():
     index = here.parent.parent / "index.html"
     with index.open() as f:
         return HttpResponse(f.read())
 
 
+def view_resume():
+    resume = here.parent.parent / "resume.html"
+    with resume.open() as f:
+        return HttpResponse(f.read())
+
+
+def view_projects():
+    projects = here.parent.parent / "projects.html"
+    with projects.open() as f:
+        return HttpResponse(f.read())
+
+
+def view_thoughts():
+    thoughts = here.parent.parent / "thoughts.html"
+    with thoughts.open() as f:
+        return HttpResponse(f.read())
+
+
+def view_image():
+    jpg_me = here.parent.parent / "images/me.jpg"
+    with jpg_me.open("rb") as src:
+        return HttpResponse(src.read(), content_type="image/jpeg")
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', view),
+    path('', view_index),
+    path('resume/', view_resume),
+    path('projects/', view_projects),
+    path('thoughts/', view_thoughts),
+    path('me/', view_image)
 ]
