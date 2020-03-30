47ac44bb46682e9d494e00c2e8da25e944321759
KaHcePBa <61241758+KaHcePBa@users.noreply.github.com>
2020-03-30 20:04:52 +0300

    ver.1.0.6

--- src/project/urls.py
+++ src/project/urls.py
@@ -8,26 +8,26 @@ here = Path(__file__).parent.resolve()
 
 def view_index():
     index = here.parent.parent / "index.html"
-    with index.open() as f:
-        return HttpResponse(f.read())
+    with index.open() as src:
+        return HttpResponse(src.read())
 
 
 def view_resume():
     resume = here.parent.parent / "resume.html"
-    with resume.open() as f:
-        return HttpResponse(f.read())
+    with resume.open() as src:
+        return HttpResponse(src.read())
 
 
 def view_projects():
     projects = here.parent.parent / "projects.html"
-    with projects.open() as f:
-        return HttpResponse(f.read())
+    with projects.open() as src:
+        return HttpResponse(src.read())
 
 
 def view_thoughts():
     thoughts = here.parent.parent / "thoughts.html"
-    with thoughts.open() as f:
-        return HttpResponse(f.read())
+    with thoughts.open() as src:
+        return HttpResponse(src.read())
 
 
 def view_image():
