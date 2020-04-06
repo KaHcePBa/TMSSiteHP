97ae900920a1730014a302a5f3825e32855049d9
KaHcePBa <61241758+KaHcePBa@users.noreply.github.com>
2020-04-06 19:29:06 +0300

    ver.2.0.4

--- src/project/urls.py
+++ src/project/urls.py
@@ -2,16 +2,34 @@ from pathlib import Path
 
 from django.contrib import admin
 from django.http import HttpResponse
+from django.http import HttpRequest
 from django.urls import path
 from django.conf import settings
+from django.shortcuts import render
 
-CSS_FILE: Path = settings.REPO_DIR / "css/style.css"
+CSS_FILE: Path = settings.REPO_DIR / "css" / "style.css"
 HTML_INDEX: Path = settings.REPO_DIR / "index.html"
 HTML_RESUME: Path = settings.REPO_DIR / "resume.html"
 HTML_PROJECTS: Path = settings.REPO_DIR / "projects.html"
 HTML_THOUGHTS: Path = settings.REPO_DIR / "thoughts.html"
-JPG_ME: Path = settings.REPO_DIR / "images/me.jpg"
-BACK_GND: Path = settings.REPO_DIR / "backgnd/background.jpg"
+JPG_ME: Path = settings.REPO_DIR / "images" / "me.jpg"
+BACK_GND: Path = settings.REPO_DIR / "backgnd" / "background.jpg"
+
+
+def view_index(request: HttpRequest) -> HttpResponse:
+    return render(request, "index.html")
+
+
+def view_resume(request: HttpRequest) -> HttpResponse:
+    return render(request, "resume.html")
+
+
+def view_projects(request: HttpRequest) -> HttpResponse:
+    return render(request, "projects.html")
+
+
+def view_thoughts(request: HttpRequest) -> HttpResponse:
+    return render(request, "thoughts.html")
 
 
 def read_static(fn, ct):  # fn = filename, ct = content type
@@ -25,20 +43,20 @@ def view_css(r):
     return read_static(CSS_FILE, "text/css")
 
 
-def view_index(r):
-    return read_static(HTML_INDEX, "text/html")
-
-
-def view_resume(r):
-    return read_static(HTML_RESUME, "text/html")
-
-
-def view_projects(r):
-    return read_static(HTML_PROJECTS, "text/html")
-
-
-def view_thoughts(r):
-    return read_static(HTML_THOUGHTS, "text/html")
+# def view_index(r):
+#     return read_static(HTML_INDEX, "text/html")
+#
+#
+# def view_resume(r):
+#     return read_static(HTML_RESUME, "text/html")
+#
+#
+# def view_projects(r):
+#     return read_static(HTML_PROJECTS, "text/html")
+#
+#
+# def view_thoughts(r):
+#     return read_static(HTML_THOUGHTS, "text/html")
 
 
 def view_image(r):
