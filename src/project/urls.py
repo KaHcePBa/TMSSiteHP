390427d535b2fd4ca4178d3bd7e43b0b34a36c6f
KaHcePBa <61241758+KaHcePBa@users.noreply.github.com>
2020-04-06 20:33:18 +0300

    ver.2.0.9

--- src/project/urls.py
+++ src/project/urls.py
@@ -7,12 +7,12 @@ from django.urls import path
 from django.conf import settings
 from django.shortcuts import render
 
-CSS_FILE: Path = settings.REPO_DIR / "css" / "style.css"
-HTML_INDEX: Path = settings.REPO_DIR / "index.html"
-HTML_RESUME: Path = settings.REPO_DIR / "resume.html"
-HTML_PROJECTS: Path = settings.REPO_DIR / "projects.html"
-HTML_THOUGHTS: Path = settings.REPO_DIR / "thoughts.html"
-JPG_ME: Path = settings.REPO_DIR / "images" / "me.jpg"
+# CSS_FILE: Path = settings.REPO_DIR / "css" / "style.css"
+# HTML_INDEX: Path = settings.REPO_DIR / "index.html"
+# HTML_RESUME: Path = settings.REPO_DIR / "resume.html"
+# HTML_PROJECTS: Path = settings.REPO_DIR / "projects.html"
+# HTML_THOUGHTS: Path = settings.REPO_DIR / "thoughts.html"
+# JPG_ME: Path = settings.REPO_DIR / "images" / "me.jpg"
 BACK_GND: Path = settings.REPO_DIR / "backgnd" / "background.jpg"
 
 
