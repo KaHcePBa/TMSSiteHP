7615ab643e450b411249d3f8b23ad84a05a5850c
KaHcePBa <61241758+KaHcePBa@users.noreply.github.com>
2020-04-17 21:42:37 +0300

    ver.4.2.1 - добавлен model для index

--- src/apps/index/admin.py
+++ src/apps/index/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from django.contrib.admin import ModelAdmin
+from apps.index.models import IndexIntroSubtitleInfo
 
-# Register your models here.
+@admin.register(IndexIntroSubtitleInfo)
+class IndexIntroSubtitleInfoAdminModel(ModelAdmin):
+    pass
--- src/apps/index/models.py
+++ src/apps/index/models.py
@@ -1,3 +1,12 @@
-from django.db import models
+from django.db import models as m
 
-# Create your models here.
+
+class IndexIntroSubtitleInfo(m.Model):
+    introsubtitle = m.TextField(null=True, blank=True)
+    age = m.IntegerField(null=True, blank=True)
+
+    class Meta:
+        verbose_name_plural = 'Intro Subtitle Info'
+
+    def __str__(self):
+        return f"IndexIntroSubtitleInfo(introsubtitle={self.introsubtitle})"
