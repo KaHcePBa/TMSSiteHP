e97f88d0e4a90aa7f08c7ccadc7c560706a75ee0
KaHcePBa <61241758+KaHcePBa@users.noreply.github.com>
2020-04-20 20:58:23 +0300

    ver.4.2.4 - в model для index (+views)

--- src/apps/index/views.py
+++ src/apps/index/views.py
@@ -7,9 +7,19 @@
 #     if request.method == "GET":
 #         return render(request, "index/index.html")
 from django.views.generic import TemplateView
+from apps.index.models import IndexInSubInf
 
 
 class IndexView(TemplateView):
     template_name = "index/index.html"
+    model = IndexInSubInf
+
+    def get_context_data(self, **kwargs):
+        ctx = super().get_context_data(**kwargs)
+
+        info = IndexInSubInf.objects.all()
+        ctx["ist"] = IndexInSubInf
+
+        return ctx
     # def get(self, request):
     #     return render(request, "index/index.html")
