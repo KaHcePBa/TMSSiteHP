63781e2c592c4c7115ac3e11ccd9a59853ca0bf3
KaHcePBa <61241758+KaHcePBa@users.noreply.github.com>
2020-04-20 19:50:53 +0300

    ver.4.2.2 - удалён model для index

--- src/apps/index/admin.py
+++ src/apps/index/admin.py
@@ -1,7 +1,7 @@
-from django.contrib import admin
-from django.contrib.admin import ModelAdmin
-from apps.index.models import IndexIntroSubtitleInfo
-
-@admin.register(IndexIntroSubtitleInfo)
-class IndexIntroSubtitleInfoAdminModel(ModelAdmin):
-    pass
+# from django.contrib import admin
+# from django.contrib.admin import ModelAdmin
+# from apps.index.models import IndexIntroSubtitleInfo
+#
+# @admin.register(IndexIntroSubtitleInfo)
+# class IndexIntroSubtitleInfoAdminModel(ModelAdmin):
+#     pass
