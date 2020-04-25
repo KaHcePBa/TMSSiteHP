4960e70b4e859486b2853ed724bc9d5d35b4b6d1
KaHcePBa <61241758+KaHcePBa@users.noreply.github.com>
2020-04-25 19:38:12 +0300

    ver.4.5.0 - jinja2 работает начальная страница но ссылки пока некорректно работают

--- src/project/urls.py
+++ src/project/urls.py
@@ -1,25 +1,5 @@
-# from pathlib import Path
-
 from django.contrib import admin
-# from django.http import HttpResponse
 from django.urls import path, include
-# from django.conf import settings
-
-# BACKnLOGO: Path = settings.PROJECT_DIR / "backgnd" / "intro-bg.jpg"
-
-
-# def read_static(fn, ct):  # fn = filename, ct = content type
-#     with open(fn, "rb") as src:  # открываем объект (для картинок стоит "rb" - читать в бинарном формате
-#         content = src.read()  # читаем открытый объект
-#         resp = HttpResponse(content, content_type=ct)  # response
-#         return resp  # выполняем
-
-
-# def view_backgndnlogo(r):
-#     return read_static(BACKnLOGO, "image/jpeg", "image/png")
-
-
-
 
 urlpatterns = [
     path('admin/', admin.site.urls),  # позволяет входить под админом на свой сайт sakasper.herokuapp.com/admin
@@ -27,7 +7,4 @@ urlpatterns = [
     path('resume/', include("applications.resume.urls")),
     path('projects/', include("applications.projects.urls")),
     path('thoughts/', include("applications.thoughts.urls")),
-    # path('me/', view_image),
-    # path('backgnd/', view_backgndnlogo),
-    # path('css/', view_css),
 ]
