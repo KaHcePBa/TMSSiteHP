70fa4b38c4713cbd5bcf6d21b980c729168ea0f6
KaHcePBa <61241758+KaHcePBa@users.noreply.github.com>
2020-05-03 02:32:53 +0300

    ver.4.5.3 - randomize introsubtitle

--- src/applications/index/views.py
+++ src/applications/index/views.py
@@ -6,8 +6,11 @@
 # def view_index(request: HttpRequest) -> HttpResponse:
 #     if request.method == "GET":
 #         return render(request, "index/index.html")
+import random
+
 from django.views.generic import TemplateView
 from applications.index.models import IndexInSubInf
+# from django.core.exceptions import ObjectDoesNotExist
 
 
 class IndexView(TemplateView):
@@ -17,8 +20,19 @@ class IndexView(TemplateView):
     def get_context_data(self, **kwargs):
         ctx = super().get_context_data(**kwargs)
 
-        info = IndexInSubInf.objects.first()
+        # try:
+        #     randomized_id = IndexInSubInf.objects.get(id=5)
+        #     randomized_id = IndexInSubInf.objects.get(id=4)
+        # except ObjectDoesNotExist:
+        #     pass
+
+        randomized_id = random.randrange(1, 4) # если нужны от 1 до 3
+
+        info = IndexInSubInf.objects.get(id=randomized_id)
         ctx["ist"] = info.ist
+
+        if ctx["ist"] is None:
+            ctx["ist"] = 'Your database is empty. Please update!'
 
         return ctx
     # def get(self, request):
