0ab6e199285859fb13ca321a1d27cc463f52f0a5
KaHcePBa <61241758+KaHcePBa@users.noreply.github.com>
2020-05-25 01:23:03 +0300

    ver.5.3.5 - scripts+

--- src/project/utils/consts.py
+++ src/project/utils/consts.py
@@ -19,27 +19,27 @@ SCRIPT_GOOGLE_ANALYTICS = """
 </script>
 """
 
-# SCRIPT_GOOGLE_TAG_MANAGER_HEAD = """
-# <!-- Google Tag Manager -->
-# <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-# new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-# j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-# 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-# })(window,document,'script','dataLayer','GTM-MJK77Z9');</script>
-# <!-- End Google Tag Manager -->
-# """
-#
-# SCRIPT_GOOGLE_TAG_MANAGER_BODY = """
-# <!-- Google Tag Manager (noscript) -->
-# <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MJK77Z9"
-# height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-# <!-- End Google Tag Manager (noscript) -->
-# """
-#
-# SCRIPT_GOOGLE_TAG_MANAGER = {
-#     "head": SCRIPT_GOOGLE_TAG_MANAGER_HEAD,
-#     "body": SCRIPT_GOOGLE_TAG_MANAGER_BODY,
-# }
+SCRIPT_GOOGLE_TAG_MANAGER_HEAD = """
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-NF4DP6Q');</script>
+<!-- End Google Tag Manager -->
+"""
+
+SCRIPT_GOOGLE_TAG_MANAGER_BODY = """
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NF4DP6Q"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+"""
+
+SCRIPT_GOOGLE_TAG_MANAGER = {
+    "head": SCRIPT_GOOGLE_TAG_MANAGER_HEAD,
+    "body": SCRIPT_GOOGLE_TAG_MANAGER_BODY,
+}
 
 SCRIPT_YANDEX_METRIKA = """
     <!-- Yandex.Metrika counter -->
