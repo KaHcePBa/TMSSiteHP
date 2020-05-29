d5fa78d78a15cce26f154a337a266b7bc1f4a8f7
KaHcePBa <61241758+KaHcePBa@users.noreply.github.com>
2020-05-29 14:06:01 +0300

    ver.5.3.8 - small corrections

--- src/project/utils/xtemplates.py
+++ src/project/utils/xtemplates.py
@@ -13,7 +13,7 @@ def big_brother(_request: Optional[HttpRequest] = None) -> Dict[str, str]:
 
     return {
         "google_analytics": consts.SCRIPT_GOOGLE_ANALYTICS,
-        "google_tag_manager": consts.SCRIPT_GOOGLE_TAG_MANAGER,
+        # "google_tag_manager": consts.SCRIPT_GOOGLE_TAG_MANAGER,
         "yandex_metrika": consts.SCRIPT_YANDEX_METRIKA,
         "google_tag_manager_head": consts.SCRIPT_GOOGLE_TAG_MANAGER_HEAD,
         "google_tag_manager_body": consts.SCRIPT_GOOGLE_TAG_MANAGER_BODY,
