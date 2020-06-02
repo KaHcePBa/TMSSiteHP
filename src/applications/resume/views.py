ba7a44e742c273acee0d530fa5c42577910ebd2c
KaHcePBa <61241758+KaHcePBa@users.noreply.github.com>
2020-06-02 21:57:09 +0300

    ver.7.0.0 - upload files via dropbox

--- src/applications/resume/views.py
+++ src/applications/resume/views.py
@@ -1,9 +1,3 @@
-# from django.http import HttpRequest, HttpResponse
-# from django.shortcuts import render
-# from django.views import View
-
-# def view_resume(request: HttpRequest) -> HttpResponse:
-#     return render(request, "resume/index.html")
 from django.views.generic import TemplateView
 
 
